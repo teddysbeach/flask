6ef5cdbbb09b3e5e20015bfcfceb92da33e1f088
Teddy <vioin8449@gmail.com>
2020-10-03 18:07:54 +0900

    implemented registeration

--- langhelper/app/__init__.py
+++ langhelper/app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'this is my secretkey which is i dont know why this one is here'
+app.config['SECRET_KEY'] = 'YOUR OWN SECRET KEY'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
--- langhelper/app/views.py
+++ langhelper/app/views.py
@@ -1,6 +1,7 @@
-from app import app
+from app import app, db
 from app.models import User, Post
-from flask import render_template
+from flask import render_template, request
+
 
 @app.route('/')
 def hello_world():
@@ -14,3 +15,17 @@ def showlists():
 def showUsers():
     users = User.query.all()
     return render_template("users.html", users=users)
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'GET':
+        return render_template("register.html")
+    else:
+        username = request.form['username']
+        nickname = request.form['nickname']
+        password = request.form['password']
+        email = request.form['email']
+        user = User(username, nickname, password, email)
+        db.session.add(user)
+        db.session.commit()
+        return render_template("users.html")    
